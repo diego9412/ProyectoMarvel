502784d9bf1bdf407c71740ae9821774adda5cdd
Diego Alejandro <dalejandro10@hotmail.com>
2023-12-06 18:57:18 -0500

    About Us y Contact Us

--- src/components/Backgrounds/AboutUs.jsx
+++ src/components/Backgrounds/AboutUs.jsx
@@ -6,6 +6,33 @@ const AboutUs = () => {
   return (
     <main className="container">
       <h1>About Us</h1>
+      <p className="fs-2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni sunt
+        sit, laudantium numquam eveniet iusto! Perspiciatis, eius? Quis id odio,
+        modi deserunt amet similique nisi beatae, labore consequuntur qui
+        recusandae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Facere odio natus veniam, autem excepturi placeat inventore velit
+        accusamus officiis odit obcaecati incidunt quam, atque cumque non illo.
+        Nisi, atque repellat.
+      </p>
+      <p className="fs-2">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex rem
+        dignissimos eaque quod perferendis temporibus? Sapiente quas, maiores
+        iure accusamus quam, consequuntur voluptatum a impedit atque, obcaecati
+        dolorum adipisci natus! Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Repudiandae iusto, maxime quaerat eveniet distinctio
+        omnis, similique quam nam nihil corrupti itaque doloremque, odit
+        accusamus quod laboriosam officiis consequatur labore iste?
+      </p>
+      <p className="fs-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ducimus
+        esse temporibus quis nulla repellendus omnis at alias quas quam eligendi
+        iure magni, nostrum fugit sunt cum. Sunt, sapiente distinctio. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Repellendus magnam
+        voluptas iste odio doloremque eaque officiis modi blanditiis, neque
+        veniam architecto pariatur aut ullam iure aliquid. Exercitationem
+        suscipit voluptas soluta!
+      </p>
     </main>
   );
 };
--- src/components/Backgrounds/ContactUs.jsx
+++ src/components/Backgrounds/ContactUs.jsx
@@ -5,6 +5,15 @@ const ContactUs = () => {
   return (
     <main className="container">
       <h1>Contact Us</h1>
+      <div className="contactUs">
+        <img src="/public/contact-us.png" alt="Contact us" />
+        <p className="fs-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, dolorem
+          consequatur veniam cupiditate sapiente velit laboriosam placeat
+          ratione obcaecati reiciendis voluptatem sit autem quo asperiores
+          dolor. Deleniti facilis sit exercitationem.
+        </p>
+      </div>
     </main>
   );
 };
